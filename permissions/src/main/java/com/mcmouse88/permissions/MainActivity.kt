ada7869165d87eeb1fdf3f3dd34d991ed3673181
mcmouse88 <mcmouse88@icloud.com>
2022-08-07 18:59:19 +0200

    simple request with Activity Result Api

--- permissions/src/main/java/com/mcmouse88/permissions/MainActivity.kt
+++ permissions/src/main/java/com/mcmouse88/permissions/MainActivity.kt
@@ -7,6 +7,8 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -14,6 +16,24 @@ import androidx.core.content.ContextCompat
 import com.mcmouse88.permissions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * для того, чтобы запращивать permissions через Activity Result Api, нужно создать два
+     * лаунчера. Так как первый запрашивает только одно разрешение, то нужно передать
+     * в качестве парметра [RequestPermission], в таком случае в параметр лямбду попадет згначение
+     * типа Boolean. Для запроса массива permissions(как во втором случае), нужно использовать
+     * [RequestMultiplePermissions], который будет в лямбду передавать Map, где ключ само
+     * разрешение, а значение Boolean.
+     */
+    private val permissionCameraAccessLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+        ::gotPermissionResultForCamera
+    )
+
+    private val permissionLocationAndRecordAudioLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions(),
+        ::gotPermissionResultForLocationAndRecordAudio
+    )
 
     /**
      * для того чтобы запросить разрешение желательно сначала проверить не было ли оно дано ранее.
@@ -32,63 +52,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonFutureOne.setOnClickListener {
-
-            if ((ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
-                        PackageManager.PERMISSION_GRANTED)
-            ) {
-                onCameraPermissionGranted()
-            } else {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.CAMERA),
-                    REQUEST_CODE_FOR_CAMERA_ACCESS
-                )
-            }
+            permissionCameraAccessLauncher.launch(Manifest.permission.CAMERA)
         }
 
         binding.buttonFeatureTwo.setOnClickListener {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO),
-                REQUEST_CODE_FOR_RECORD_AUDIO_ACCESS
+            permissionLocationAndRecordAudioLauncher.launch(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO)
             )
-        }
-    }
-
-    /**
-     * Метод для обработки результатов ответа пользователя при подтверждении или отклонении
-     * permission. Первый аргумент requestCode, который мы получаем при запрашивании разрешения,
-     * второй массив permissions, который мы запрашивали, а третий результаты ответа пользователя
-     * представленные в виде массива, содержащие в себе Int с константами (PERMISSION_GRANTED,
-     * PERMISSION_DENIED и т.д.), по идексам соотвествующие массиву второго параметра. У отклонения
-     * разрешения также есть два типа, отклонение навсегда, и отклонение с возможностью повторно
-     * запросить разрешение. Для того, чтобы проверить не заблокировал ли пользователь навсегда
-     * разрешение на доступ к чему-либо используестя метод [shouldShowRequestPermissionRationale].
-     * Если метод возвращает true, то значит можно запросить еще раз предоставление разрешения.
-     */
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            REQUEST_CODE_FOR_CAMERA_ACCESS -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    onCameraPermissionGranted()
-                } else {
-                    if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
-                        Toast.makeText(this, getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
-                    } else {
-                        askForAccessPermission()
-                    }
-                }
-            }
-            REQUEST_CODE_FOR_RECORD_AUDIO_ACCESS -> {
-                if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
-                    Toast.makeText(this, getString(R.string.rec_audio_and_location_granted), Toast.LENGTH_SHORT).show()
-                }
-            }
         }
     }
 
@@ -115,6 +85,24 @@ class MainActivity : AppCompatActivity() {
                 }
                 .create()
                 .show()
+        }
+    }
+
+    private fun gotPermissionResultForCamera(granted: Boolean) {
+        if (granted) {
+            onCameraPermissionGranted()
+        } else {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                Toast.makeText(this, getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
+            } else {
+                askForAccessPermission()
+            }
+        }
+    }
+
+    private fun gotPermissionResultForLocationAndRecordAudio(grantResults: Map<String, Boolean>) {
+        if (grantResults.entries.all { it.value }) {
+            Toast.makeText(this, getString(R.string.rec_audio_and_location_granted), Toast.LENGTH_SHORT).show()
         }
     }
 
